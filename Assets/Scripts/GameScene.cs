a05e6ff9fff82172f958fec1b5188a92a7787a5d
JuddFloriza <juddkristian@gmail.com>
2016-08-19 12:38:38 +0800

    Import Changes
    Scene manager now working

---
+++ Assets/Scripts/GameScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScene : Scene
+{
+    public override void initializeScene(params object[] param)
+    {
+
+    }
+
+    public override bool hasInitializeScene()
+    {
+        return true;
+    }
+}
